1b0b7ecdf01577c42ce10fa4680594b440ba0388
Omar <ohammad@gwu.edu>
2023-09-27 07:15:50 -0400

    finished meta corgis

--- practice-for-ch-sql-meta-corgis-main/meta_corgis.rb
+++ practice-for-ch-sql-meta-corgis-main/meta_corgis.rb
@@ -108,7 +108,7 @@ class MetaCorgiSnacks
     @box_id = box_id
   end
 
-  #phase 1
+  # phase 1
   def method_missing(name, *args)
     info = @snack_box.send("get_#{name}_info", @box_id)
     tastiness = @snack_box.send("get_#{name}_tastiness", @box_id)
